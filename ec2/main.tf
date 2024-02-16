38bdd97c569137ef04a19fa955408e7521105428
clanda20 <clanda@hotmail.com>
2024-02-16 14:50:04 -0800

    adding Debian 12 to ec2

--- ec2/main.tf
+++ ec2/main.tf
@@ -39,20 +39,20 @@ resource "aws_security_group" "drines_ucsc_test" {
   }
 }
 
-data "aws_ami" "ubuntu" {
+data "aws_ami" "debian-11" {
   most_recent = true
 
   filter {
     name = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+    values = ["debian-11-amd64-*"]
   }
 
-  owners = ["099720109477"]
+  owners = ["136693071363"]
 }
 
 
 resource "aws_instance" "app_server" {
-  ami           = data.aws_ami.ubuntu.id
+  ami           = data.aws_ami.debian-11.id
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.drines_ucsc_test.id]
   key_name = "testing_aws_key"
